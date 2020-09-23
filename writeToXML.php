b773b025810b95a2f4b87f82b64d611de6e469db
sridgr8 <sridgr8.13@gmail.com>
2020-09-24 01:28:31 +0530

    Initial Commit

---
+++ writeToXML.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_REQUEST['ok'])){
+  $xml = new DOMDocument("1..0","UTF-8");
+  $xml->load("testData.xml");
+  $rootTag = $xml->getElementsByTagName("accounts")->item(0);
+  $dataTag = $xml->createElement("user");
+  $usernameTag = $xml->createElement("username",$_REQUEST['username']);
+  $passwordTag = $xml->createElement("password",$_REQUEST['password']);
+  $firstnameTag = $xml->createElement("firstname",$_REQUEST['firstname']);
+  $lastnameTag = $xml->createElement("lastname",$_REQUEST['lastname']);
+  $dataTag->appendChild($usernameTag);
+  $dataTag->appendChild($passwordTag);
+  $dataTag->appendChild($firstnameTag);
+  $dataTag->appendChild($lastnameTag);
+  $rootTag->appendChild($dataTag);
+  $xml->save("testData.xml");
+  header("Location: index.html");
+}
+?>
